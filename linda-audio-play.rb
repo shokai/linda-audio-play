3c429d01d201652de4d9bc3955e47bf987d2c576
Sho Hashimoto <hashimoto@shokai.org>
2013-05-29 17:31:30 +0900

    use em-rocketio-linda-client

--- linda-audio-play.rb
+++ linda-audio-play.rb
@@ -1,29 +1,30 @@
 #!/usr/bin/env ruby
 require 'rubygems'
-require 'sinatra/rocketio/linda/client'
+require 'em-rocketio-linda-client'
 $stdout.sync = true
 
 url   = ENV["LINDA_BASE"]  || ARGV.shift || "http://localhost:5000"
 space = ENV["LINDA_SPACE"] || "test"
-puts "connecting.. #{url}"
-linda = Sinatra::RocketIO::Linda::Client.new url
-ts = linda.tuplespace[space]
 
-linda.io.on :connect do  ## RocketIO's "connect" event
-  puts "connect!! <#{linda.io.session}> (#{linda.io.type})"
-  ts.watch ["audio", "play"] do |tuple|
-    next unless tuple.size == 3
-    next unless tuple[2] =~ /https?:\/\/.+/
-    puts url = tuple[2]
-    ts.write ["audio", "play", url, "start"]
-    tmp = "/var/tmp/audio_play.tmp"
-    system "curl #{url} > #{tmp} && afplay #{tmp} && rm #{tmp}"
-    ts.write ["audio", "play", url, "end"]
+EM::run do
+  puts "connecting.. #{url}"
+  linda = EM::RocketIO::Linda::Client.new url
+  ts = linda.tuplespace[space]
+
+  linda.io.on :connect do  ## RocketIO's "connect" event
+    puts "connect!! <#{linda.io.session}> (#{linda.io.type})"
+    ts.watch ["audio", "play"] do |tuple|
+      next unless tuple.size == 3
+      next unless tuple[2] =~ /https?:\/\/.+/
+      puts url = tuple[2]
+      ts.write ["audio", "play", url, "start"]
+      tmp = "/var/tmp/audio_play.tmp"
+      system "curl #{url} > #{tmp} && afplay #{tmp} && rm #{tmp}"
+      ts.write ["audio", "play", url, "end"]
+    end
+  end
+
+  linda.io.on :disconnect do
+    puts "RocketIO disconnected.."
   end
 end
-
-linda.io.on :disconnect do
-  puts "RocketIO disconnected.."
-end
-
-linda.wait
